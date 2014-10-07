012ae5ee5da86992195a2b7591fe795d26e3c402
Tobias Bengfort <tobias.bengfort@gmx.net>
2014-10-07 20:06:21 +0200

    use curses by default

--- dirtywords/__init__.py
+++ dirtywords/__init__.py
@@ -1,10 +1,10 @@
 # flake8: noqa
 
 try:
-    from pygame_core import Screen
+    from curses_core import Screen
 except ImportError:
     try:
-        from curses_core import Screen
+        from pygame_core import Screen
     except ImportError:
         from stupid_core import Screen
 
